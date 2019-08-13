8907e5a6943c245f3f2ba20a32a5cb7306907bce
Corey Short <cshort012@gmail.com>
2019-08-12 19:11:13 -0700

    Added component constructor.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    render(){
-        return(
-            <div>
-                <h1>Hello World!</h1>
-            </div> 
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
