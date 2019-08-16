4276dcc38ebeca719e2e906a378afd4950eeff19
Corey Short <cshort012@gmail.com>
2019-08-16 12:32:24 -0700

    Changed from a class component to a stateless functional component. Currently, using a class isn't needed, so there is no need for the extra overhead and code using a class component brings with it.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,18 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
-}
-
+const App = () => {
+  const greeting = 'Hello World!';
+  return <h1>{greeting}</h1>;
+};
 export default App;
