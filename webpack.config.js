0b7875cb12e542703d42880adc399e95beef4951
Corey Short <cshort012@gmail.com>
2019-08-16 12:35:20 -0700

    Configured webpack to resolve jsx extensions along with js. Updated the entrypoint. Removed hot reload command from package.json and added it to devServer config.

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
         path: path.join(__dirname, '/build'),
         filename: 'index_bundle.js'
     },
     devServer: {
+        hot: true,
         inline: true,
         port: 8001
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader'
                 }
             }
         ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
     },
     plugins: [
         new HtmlWebpackPlugin({
