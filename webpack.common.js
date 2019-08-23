4077f95db2c8f55ab64f71054c83fe3983378c84
Corey Short <cshort012@gmail.com>
2019-08-23 14:01:19 -0700

    chore: removed devServer from webpack-common config

--- webpack.common.js
+++ webpack.common.js
@@ -2,33 +2,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: path.join(__dirname, '/build'),
-        filename: 'index_bundle.js'
-    },
-    devServer: {
-        hot: true,
-        inline: true,
-        port: 8001
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        })
-    ]
-}
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
